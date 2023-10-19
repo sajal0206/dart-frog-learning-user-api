378d61e49523e6b4f1bcfe9f0305635a53256593
Sajal Chandaiya <sajal@rashailinfotech.co.in>
2023-10-19 10:02:36 +0530

    added code to create user in database

---
+++ lib/constants/connection.dart
@@ -0,0 +1,2 @@
+/// database connection url
+const connection = 'mysql://root:@localhost:3306/dartFrogDb?schema=public';
--- routes/user/index.dart
+++ routes/user/index.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
+import 'package:tasklist_backend/constants/connection.dart';
+import 'package:tasklist_backend/src/generated/prisma/prisma_client.dart';
 import 'package:tasklist_backend/user/user_model.dart';
 import 'package:tasklist_backend/users.dart';
 
@@ -18,40 +21,75 @@ Response _getUsers() {
   );
 }
 
+/// to add user directly into the list
+// Future<Response> _addNewUser(RequestContext context) async {
+//   final data = jsonDecode(await context.request.body()) as Map<String, dynamic>;
+//   if (data['id'] != null &&
+//       data['name'] != null &&
+//       data['age'] != null &&
+//       data['email'] != null) {
+//     final userModel = UserModel.fromJson(data);
+//     if (users.any((element) => element.id == userModel.id)) {
+//       return Response.json(
+//         body: {
+//           'message':
+//               'id ${userModel.id} already exists please choose any other',
+//         },
+//       );
+//     } else {
+//       users.add(userModel);
+//       return Response.json(
+//         body: {
+//           'message': 'User Added Successfully',
+//           'user': userModel.toJson(),
+//         },
+//       );
+//     }
+//   } else {
+//     return Response.json(
+//       body: {
+//         'message': 'Please provide all required fields',
+//         'fields': {
+//           'id': 'Required with type String/ Character',
+//           'name': 'Required with type String/ Character',
+//           'age': 'Required with type double/ float',
+//           'email': 'Required with type String/ Character',
+//         },
+//       },
+//     );
+//   }
+// }
+
+/// create user into the database
 Future<Response> _addNewUser(RequestContext context) async {
   final data = jsonDecode(await context.request.body()) as Map<String, dynamic>;
-  if (data['id'] != null &&
-      data['name'] != null &&
-      data['age'] != null &&
-      data['email'] != null) {
+  if (data['name'] != null && data['age'] != null && data['email'] != null) {
     final userModel = UserModel.fromJson(data);
-    if (users.any((element) => element.id == userModel.id)) {
-      return Response.json(
-        body: {
-          'message':
-              'id ${userModel.id} already exists please choose any other',
-        },
-      );
-    } else {
-      users.add(userModel);
-      return Response.json(
-        body: {
-          'message': 'User Added Successfully',
-          'user': userModel.toJson(),
-        },
-      );
-    }
+    // users.add(userModel);
+    final prisma = PrismaClient(
+      datasources: const Datasources(
+        db: connection,
+      ),
+    );
+    await prisma.user.create(data: UserCreateInput.fromJson(data));
+    return Response.json(
+      body: {
+        'message': 'User Added Successfully',
+        'user': userModel.toJson(),
+      },
+      statusCode: HttpStatus.accepted,
+    );
   } else {
     return Response.json(
       body: {
         'message': 'Please provide all required fields',
         'fields': {
-          'id': 'Required with type String/ Character',
           'name': 'Required with type String/ Character',
           'age': 'Required with type double/ float',
           'email': 'Required with type String/ Character',
         },
       },
+      statusCode: HttpStatus.conflict,
     );
   }
 }
